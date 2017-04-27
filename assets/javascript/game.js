2de652b9d7671805392532d9541df77b677822c0
Omnomon <jessselu@gmail.com>
2017-04-27 15:44:10 -0700

    hangman code works, but need to get it to go to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var underscores =[];
 
 //---------------------------------------------
 var testWord = 	"rEdRedred"; 
-var testLetter ="E";
+/*var testLetter ="E";*/
 var indices = [];
 //---------------------------------------------
 
@@ -51,7 +51,7 @@ var testArray = testWord.split("");
 //Display the html ** placeholder ** unde
 window.onload = function play() {
 
-	document.getElementById("hangman").innerHTML = blankWord;
+/*	document.getElementById("hangman").innerHTML = blankWord;*/
 
 } 
 //---------------------------------------------
@@ -61,19 +61,18 @@ window.onload = function play() {
 
 // Declare keystrokes
 
-var userGuess= ""
-document.onkeyup = function (event) {
+var userGuess = "";
+/*document.onkeyup = function (event) {
 
 //define keycode as var 
 userGuess = event.key;
 console.log("current letter is " + userGuess)
-return userGuess = ""
+return userGuess;
 
-}
+}*/
 
 //---------------------------------------------
 
-//replace testLetter with testWord 
 
 
 //---------------------------------------------
@@ -81,43 +80,65 @@ return userGuess = ""
 
 //write a function that will find all instances of a letter for a given word
 
-function checkTestLetter(testLetter, testWord) {
+function checkTestLetter(userGuess, testWord) {
 	indices = [];
-	testLetter = testLetter.toLowerCase()
+	userGuess = userGuess.toLowerCase()
 	testWord = testWord.toLowerCase()
 	var testWordLetters = testWord.split("")
-	var letterIndex = testWordLetters.indexOf(testLetter);
+	var letterIndex = testWordLetters.indexOf(userGuess);
 	while (letterIndex != -1) {
 		indices.push(letterIndex);
-		letterIndex = testWordLetters.indexOf(testLetter, letterIndex + 1);
+		letterIndex = testWordLetters.indexOf(userGuess, letterIndex + 1);
 		} 
 	return indices
 }
 
 // ----------------------------------------------------------------------------
 
+//--------- check run ----------
 var userGuessRightAt = checkTestLetter('e', 'redredred')
 console.log(userGuessRightAt)
+//------------------------------------------
 
 
-//replace blankWord with testLetter 
-function replaceUnderScoreWithTestLetter() {
+//if userGuess is right, replace blankWord with testLetter 
+function replaceUnderScoreWithTestLetter(userGuessRightAt) {
 	for (var counter = 0; counter < userGuessRightAt.length; counter++) {
-		blankWord[userGuessRightAt[counter]] = testLetter[0];
+		blankWord[userGuessRightAt[counter]] = userGuess[0];
 	}
 }
 
 // -------- 
 
 
-// if userGuess is wrong, push to wrong list 
+// if userGuess is wrong, increase count, add to userStrikes 
 var userStrikes = [];
 var count = 0;
 function userGuessedWrong(userGuessRightAt) {
 	if (userGuessRightAt.length === 0) {
-		userStrikes.push(testLetter);
+		userStrikes.push(userGuess);
 		count++ ;
 	}
 
 	return count
 }
+
+$(document).keyup(function() {
+	userGuess = event.key;
+	console.log("current letter is " + userGuess);
+	var userGuessRightAt = checkTestLetter(userGuess, testWord)
+	console.log(userGuessRightAt)
+		if (userGuessRightAt.length ===0) {
+			userStrikes.push(userGuess);
+			count += 1 
+		
+		} else {
+			for (var counter = 0; counter < userGuessRightAt.length; counter++) {
+			blankWord[userGuessRightAt[counter]] = userGuess;
+			}
+		}
+
+	console.log(blankWord)
+	console.log(count)
+
+})
