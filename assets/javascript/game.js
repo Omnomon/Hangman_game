06bd82d126b138caa5a29023f273dce5a4c03e4d
Omnomon <jessselu@gmail.com>
2017-04-29 03:08:13 -0700

    im done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,25 +11,31 @@ var underscores =[];
 
 
 //---------------------------------------------
-var testWord = 	"rEdRedred"; 
+var testWordBank =  [ 
+"This.screw()", 
+"curry",
+"my_back_hurts", 
+"homework", 
+"apples" , 
+"legolas", 
+"winter_is_coming",
+"smoke_weed_every_day",
+"i_made_a_game", 
+"i hope this does not break the game"]; 
+
+var testWord = testWordBank[Math.floor(Math.random() * 10)]
 /*var testLetter ="E";*/
 var indices = [];
 //---------------------------------------------
 
+/*
 
-
-//steal contains function 
-
-var contains = function (haystack, needle) {
-	return !!~haystack.indexOf(needle)
-};
-
-
+*/
 //---------------------------------------------
 underscores = underscore.repeat(testWord.length);
 var blankWord = underscores.split("  ");
 console.log(underscores);
-console.log(blankWord)
+console.log(blankWord, blankWord.length)
 //---------------------------------------------
 
 
@@ -39,39 +45,25 @@ console.log(blankWord)
 
 testWord = testWord.toLowerCase()
 var testArray = testWord.split("");
+console.log(testArray , testArray.length)
 
 //---------------------------------------------
 
+//create array of images 
+
+var imageArray = [
+'./assets/images/platform.png', 
+'./assets/images/strike_1.png', 
+'./assets/images/strike_2.png', 
+'./assets/images/strike_3.png', 
+'./assets/images/strike_4.png', 
+'./assets/images/strike_5.png', 
+'./assets/images/strike_6.png'
+]; 
 
 
-
-//---------------------------------------------
-
-
-//Display the html ** placeholder ** unde
-window.onload = function play() {
-
-/*	document.getElementById("hangman").innerHTML = blankWord;*/
-
-} 
-//---------------------------------------------
-
-
-//---------------------------------------------
-
-// Declare keystrokes
 
 var userGuess = "";
-/*document.onkeyup = function (event) {
-
-//define keycode as var 
-userGuess = event.key;
-console.log("current letter is " + userGuess)
-return userGuess;
-
-}*/
-
-//---------------------------------------------
 
 
 
@@ -94,6 +86,33 @@ function checkTestLetter(userGuess, testWord) {
 }
 
 // ----------------------------------------------------------------------------
+
+// -------
+
+/*function checkUnderscores(underscore, testWord) {
+	console.log("checkUnderscores running")
+	var underscoreIndex = [];
+	while (underscoreIndex != -1) {
+		underscoreIndex = blankWord.indexOf(underscore, underscoreIndex +1)
+		if ( underscoreIndex.length == 0 ) {
+			$("#letter_correct").html("YOU WIN")
+			console.log("checkUnderscores you win")
+		if ( underscoreIndex.length > 0  && count == 6) {
+			$("#letter_incorrect").append (" YOU LOSE ")
+			console.log("checkUnderscores you lose ")
+		} 
+		}
+	}
+}
+*/
+
+function checkBlankWordEqualsTestWord (blankWord, testWord) {
+	for (var i = 0; i <blankWord.length; i++) {
+		if (blankWord[i] == testWord[i]) {
+			$("#letter_correct").html("YOU WIN")
+		}
+	} 
+}
 
 //--------- check run ----------
 var userGuessRightAt = checkTestLetter('e', 'redredred')
@@ -123,22 +142,74 @@ function userGuessedWrong(userGuessRightAt) {
 	return count
 }
 
-$(document).keyup(function() {
+// put all the code together 
+
+window.onload = function play() {
+	$("#letter_correct").append($("<div>").html(blankWord.length + " characters (may or may not include other characters!)"))
+} 
+
+
+
+$(document).ready().keyup(function() {
 	userGuess = event.key;
 	console.log("current letter is " + userGuess);
-	var userGuessRightAt = checkTestLetter(userGuess, testWord)
-	console.log(userGuessRightAt)
+	var userGuessRightAt = checkTestLetter(userGuess, testWord);
+	console.log(userGuessRightAt);
 		if (userGuessRightAt.length ===0) {
-			userStrikes.push(userGuess);
-			count += 1 
-		
-		} else {
-			for (var counter = 0; counter < userGuessRightAt.length; counter++) {
-			blankWord[userGuessRightAt[counter]] = userGuess;
-			}
-		}
+			userGuessedWrong(userGuessRightAt);
+			$("#noose").attr("src", imageArray[count])
+				if (count === 6) {
+				$('#letter_incorrect').html("<div>GAME OVER</div>")
+				$('#letter_correct').hide()
+				$('#reset').html("Play again?")
+				$('#theWord').html(testWord)
+				};
 
-	console.log(blankWord)
-	console.log(count)
+			};
+	replaceUnderScoreWithTestLetter(userGuessRightAt);
 
-})
+	$("#theWord").html(blankWord);
+	$("#letter_incorrect").html("You have " + ( 6 - count ) + " guesses left. Letters already guessed: " + userStrikes);
+	if ( count >= 6 ) {
+		$("#letter_incorrect").html("YOU LOSE");
+	};
+
+
+	$("#letter_correct").text(underscores).html(blankWord).append("<div>");
+	
+	console.log(blankWord);
+	console.log(count);
+
+}); 
+
+$(document).ready(function(){
+	$("#reset").click(function(){
+		console.log("reset button")
+		window.location.reload(true)
+	});
+});
+	
+
+/*	if (userGuessRightAt.length !==0) {
+		$("#letter_correct").append($("<div>").html(userGuess)).addClass("col-xs-1")
+	};
+*/
+
+
+/*			if (count +=1) {
+	 			console.log("function works")
+				var img = imageArray[0];
+				 for (var i = 0; i<imageArray.length; i++) {
+				 	if (img == imageArray[i].src) {	
+				 		img == imageArray[i+1]
+ 					} 
+
+ 					if (img == -1) {
+ 						img == imageArray[0];
+ 					} else {
+ 						console.log("fix hangmang images")
+ 					}
+
+				 } 
+*/
+
